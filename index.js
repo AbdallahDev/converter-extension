1c57f04e38657dd2641a1c9edb9dcff83720e56f
AbdallahDev <abdallah.dev85@gmail.com>
2026-01-25 11:04:48 +0000

    stage

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 let conversionValue = 20;
-
 const valueInputEl = document.getElementById("valueInput");
+const lengthCardEl = document.getElementById('lengthCard')
+const volumeCardEl = document.getElementById('volumeCard')
+const massCard
 
 valueInputEl.value = conversionValue;
+
